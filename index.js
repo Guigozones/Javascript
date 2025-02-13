606e0d26d1a27c1ecbd6d42d63531d34bcf6d2b1
Guigozones <guilhermeoliveira7962@gmail.com>
2025-02-13 19:29:06 -0300

    aprendendo objeto

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let price = 30;
-console.log(price);
+
+let itemName = 'pen'
+let itemPrice = 3
+let itemAvailable = true
+let itemColor = undefined 
+itemPrice = 'dois'
+
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true, 
+    itemColor: 'Red'
+}
+
+console.log(pen)
+
+pen.itemColor = 'blue'
+
+console.log(pen.itemColor)
