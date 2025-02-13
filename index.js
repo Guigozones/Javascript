caea334860dc93aad9b663f574332255a170955b
Guigozones <guilhermeoliveira7962@gmail.com>
2025-02-13 19:36:17 -0300

    array em JS

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-
-let itemName = 'pen'
-let itemPrice = 3
-let itemAvailable = true
-let itemColor = undefined 
-itemPrice = 'dois'
-
-let pen = {
-    itemName: 'Pen',
-    itemPrice: 3,
-    itemAvailable: true, 
-    itemColor: 'Red'
-}
-
-console.log(pen)
-
-pen.itemColor = 'blue'
-
-console.log(pen.itemColor)
+let friends = ['Marcos', 'Pedro', 'João']
+friends[2] = 'Geraldo'
+console.log(friends)
